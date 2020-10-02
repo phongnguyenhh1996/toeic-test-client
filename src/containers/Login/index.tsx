2b273d728a02b803af36ef978f908ac462bcbe34
Phong Nguyen <phongnhh@Phongs-Mac-mini.local>
2020-10-02 16:59:02 +0700

    style login screen

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import { Styled } from "./styled"
+import LOGO from "../../assets/images/TOEIC_ICON.svg"
+import { TextField, InputAdornment, Button } from '@material-ui/core'
+import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
+import bgRightSide from "../../assets/images/learning-english-online.jpg"
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Styled.Login>
+      <Styled.Login__Inner>
+        <Styled.Login__Side>
+          <img width={180} height={100} src={LOGO} alt="Logo" />
+          <Styled.Login__Form>
+            <TextField
+              autoFocus
+              id="input-with-icon-textfield"
+              label="Username"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AiOutlineUser />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              id="input-with-icon-textfield"
+              label="Password"
+              type="password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AiOutlineLock />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </Styled.Login__Form>
+
+        </Styled.Login__Side>
+        <Styled.Login__Side right>
+          <Styled.Login__Right>
+            <img width="100%" src={bgRightSide} alt="Right Banner Background" />
+          </Styled.Login__Right>
+        </Styled.Login__Side>
+      </Styled.Login__Inner>
+    </Styled.Login>
   )
 }
 
--- src/containers/Login/styled.ts
+++ src/containers/Login/styled.ts
@@ -1,9 +1,54 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+interface SideProps {
+  right?: boolean
+}
 
 const Login = styled.div`
   background-color: #F2F2F2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Login__Inner = styled.div`
+  max-width: 1100px;
+  background-color: #fff;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  display: flex;
+  flex: 1;
+  border-radius: 20px;
+  overflow: hidden;
+`
+
+const Login__Side = styled.div<SideProps>`
+  flex: 0 0 50%;
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${props => !props.right && css`
+    padding: 40px;
+  `}
+  & > *:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`
+const Login__Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > *:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`
+
+const Login__Right = styled.div`
 `
 
 export const Styled = {
-  Login
+  Login,
+  Login__Inner,
+  Login__Side,
+  Login__Right,
+  Login__Form
 }
