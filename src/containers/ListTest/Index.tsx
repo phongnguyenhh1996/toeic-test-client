53e5f292dcecec37f7116a025566bbc2bf4d3fde
Phong NHH <phongnguyenhh.1996@gmail.com>
2021-02-07 16:59:35 +0700

    refactor list test url, pagination

--- src/containers/ListTest/Index.tsx
+++ src/containers/ListTest/Index.tsx
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState } from 'react';
+import React,{ useCallback, useEffect, useState } from 'react';
 import { Grid, Container } from "@material-ui/core";
 import { PaperListTest, TabsListTest, TabListTest, ContainerPagin, GridListTest } from './style';
 import { TestItem } from '../Dashboard/components/TestItem';
@@ -7,16 +7,16 @@ import { listTestRequest } from '../../actions/list_Test';
 import { usePrevious } from '../../utils/hooks';
 import { range } from 'lodash';
 import PaginationLink from './TestPagtion';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import DetailTestItem from './DetailTestItem';
 import { Test } from '../../utils/function';
 
 
-const TAB_TYPE: any = {
-    0: 'all',
-    1: 'created',
-    2: 'completed'
-}
+const TAB_TYPE: any = [
+    'all',
+    'created',
+    'completed'
+]
 const initiTestItemDetail:Test = {
     name: "",
     description: "",
@@ -31,13 +31,13 @@ const initiTestItemDetail:Test = {
     questions:{},
 }
 const ListTest: React.FC = () => {
-    const [tabIndex, setTabIndex] = useState(0);
     const [isOpenDialog,setIsOpenDialog] = useState(false);
-    const [testItemDetail,settestItemDetail] = useState<Test>(initiTestItemDetail);
-    const listTest = useSelector((state:any)=> state.listTest) ;
-    const isLoading = useSelector((state:any)=> state.listTest.loading) ;
-    const dispatch = useDispatch();
+    const [testItemDetail,settestItemDetail] = useState<Test>(initiTestItemDetail)
+    const listTest = useSelector((state:any)=> state.listTest)
+    const isLoading = useSelector((state:any)=> state.listTest.loading)
+    const dispatch = useDispatch()
     const location = useLocation()
+    const history = useHistory()
     
     
     const changeIsOpenDialog = (test: any) =>{
@@ -48,28 +48,37 @@ const ListTest: React.FC = () => {
     const closeDialog = () => {
         setIsOpenDialog(false);
     }
-
-    const handleChange = (event: any, newValue: number) => {
-        setTabIndex(newValue);
-    }
-    const prevTabIndex = usePrevious(tabIndex)
-    const testsObject = listTest[TAB_TYPE[tabIndex]]
+    
+    const query = new URLSearchParams(location.search)
+    const tabType = query.get('type') || '0'
+    const prevTabType = usePrevious(tabType)
+    const page = parseInt(query.get('page') || '1', 10);
+    const prevPage = usePrevious(page)
+    const testsObject = listTest[TAB_TYPE[tabType]]
     const testsData = testsObject?.tests
     const totalPage = testsObject?.totalPages
-    const query = new URLSearchParams(location.search);
-    const page = parseInt(query.get('page') || '1', 10) - 1;
-    const prevPage = usePrevious(page)
+    
     
     useEffect(()=>{
-        if ((tabIndex !== prevTabIndex && !testsData) || (page !== prevPage)) {
-            dispatch(listTestRequest(TAB_TYPE[tabIndex], page));
+        if ((tabType !== prevTabType && !testsData) || (page !== prevPage)) {
+            dispatch(listTestRequest(TAB_TYPE[tabType], page));
         }
-    },[prevTabIndex, tabIndex, testsData, dispatch, page, prevPage])
+    },[prevTabType, tabType, testsData, dispatch, page, prevPage])
 
     useEffect(()=>{
-        dispatch(listTestRequest(TAB_TYPE[tabIndex], page));
+        dispatch(listTestRequest(TAB_TYPE[tabType], page));
     // eslint-disable-next-line
     },[])
+
+    useEffect(() => {
+        if (!tabType) {
+            history.replace('/list-test?type=0')
+        }
+    }, [tabType, history])
+
+    const handleChangeTab = useCallback((e, index) =>{
+        history.push(`/list-test?type=${index}`)
+    }, [history])
 
     return (
         <Container fixed>
@@ -77,15 +86,14 @@ const ListTest: React.FC = () => {
                 <Grid item xs={12}>
                     <PaperListTest square >
                         <TabsListTest
-                            value={tabIndex}
+                            value={parseInt(tabType)}
                             indicatorColor="primary"
                             textColor="primary"
-                            onChange={handleChange}
+                            onChange={handleChangeTab}
                         >
                             <TabListTest label="All Test" />
                             <TabListTest label="Created Test"/>
                             <TabListTest label="Completed Test"   />
-                           
                         </TabsListTest>
                     </PaperListTest>
                 </Grid>
@@ -116,7 +124,9 @@ const ListTest: React.FC = () => {
                 </GridListTest>
                 <Grid item xs={12} >
                     <ContainerPagin>
-                        <PaginationLink totalPage={totalPage}/>
+                        {totalPage > 1 &&
+                            <PaginationLink totalPage={totalPage}/>
+                        }
                     </ContainerPagin>
                 </Grid>
             </Grid>
