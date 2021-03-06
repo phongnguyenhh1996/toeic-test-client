f3a0dab0f46223360cc01009126fa2d158ddf38f
Phong NHH <phongnguyenhh.1996@gmail.com>
2021-03-06 22:49:17 +0700

    update API URL

--- src/containers/Statistic/index.tsx
+++ src/containers/Statistic/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
+import * as Styled from "./style";
 
 const Statistic: React.FC = () => {
 
   return (
     <Container fixed>
-      Statistic
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Styled.Wrapper>
+            test
+          </Styled.Wrapper>
+        </Grid>
+        <Grid item xs={3}>
+          <Styled.Wrapper>
+            test
+          </Styled.Wrapper>
+        </Grid>
+        <Grid item xs={3}>
+          <Styled.Wrapper>
+            test
+          </Styled.Wrapper>
+        </Grid>
+        <Grid item xs={3}>
+          <Styled.Wrapper>
+            test
+          </Styled.Wrapper>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
-// export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : 'https://easy-toeic-api.herokuapp.com/api'
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : 'https://easytoeic-api.herokuapp.com/api'
     // export const API_URL = 'https://easy-toeic-api.herokuapp.com/api'
-export const API_URL = 'http://localhost:8080/api'
+// export const API_URL = 'http://localhost:8080/api'
 export const UPLOAD_URL = 'https://api.cloudinary.com/v1_1/easy-toiec/auto/upload'
 export const MASTER_KEY = 'masterKey'
